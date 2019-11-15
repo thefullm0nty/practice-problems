9ac4a8234b7c62bc48d3a910f8f5a70c088f8a7c
thefullm0nty <montgomery.evan@gmail.com>
2019-11-15 11:28:44 -0800

    practice25 done

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,14 +1,34 @@
 
-function sumArray(){
-	
+function sumArray( array ){
+  var sum = null;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
-
-function fitWithinVal(){
-
+function fitWithinVal(array, target){
+//  var sum = null;
+//  var newArray = [];
+//  for (var number in array){
+//    if (sum + array[number] <= target){
+//      sum += array[number];
+//      newArray.push(array[number]);
+//    }
+//  }
 }
 
 function getAllNamesShorterThan(){
 
+array.filter(function(0, 4))
+//  var length = 0;
+//  var longest;
+//
+//  for (var index = 0; index < array.length; index++) {
+//    if array[index].length > length) {
+//      var length = array[index].length;
+//      longest = array[index];
+//    }
+//  }
 }
 
 function makeLabel(){
